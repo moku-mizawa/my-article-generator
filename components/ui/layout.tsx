ef37b2a5732048fe412e2e0239a264e82dd85ff1
{moku-mizawa} <{mokumizawa.1216@gmail.com}>
2025-01-16 21:28:33 +0900

    change layout

---
+++ components/ui/layout.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <header className="bg-gray-800 text-white p-4">
+        <nav className="container mx-auto flex justify-between">
+          <Link href="/apps/article-generator">
+            <a className="text-lg font-semibold">Article Generator</a>
+          </Link>
+          <Link href="/apps/sentence-generator">
+            <a className="text-lg font-semibold">Sentence Generator</a>
+          </Link>
+        </nav>
+      </header>
+      <main className="container mx-auto p-4">
+        {children}
+      </main>
+    </div>
+  )
+}
